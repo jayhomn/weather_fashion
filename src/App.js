8b2fc33885141dc5f12dc7fdd90188a66e1e8bb8
Jay <mantine08@gmail.com>
2020-07-20 22:45:45 -0700

    Created main page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
 
 function App() {
+  const [location, setLocation] = useState("");
+
+  useEffect(() => {
+    let input = document.getElementById("pac-input");
+
+    input.addEventListener("keyup", function (event) {
+      if (event.keyCode === 13) {
+        event.preventDefault();
+        document.getElementById("submit-btn").click();
+      }
+    });
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main-div">
+      <h1>Enter Your Location</h1>
+      <form
+        onSubmit={(event) => {
+          let div = document.getElementsByClassName("main-div");
+          div.classList.add("div-move-up");
+        }}
+      >
+        <input
+          id="pac-input"
+          className="search-bar"
+          type="text"
+          value={location}
+          onChange={(event) => {
+            setLocation(event.target.value);
+          }}
+        />
+        <input id="submit-btn" className="submit-btn" type="submit" />
+      </form>
     </div>
   );
 }
