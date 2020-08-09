3df0a1d51de4450199e8fe66848c589264b9be46
Jay <mantine08@gmail.com>
2020-08-08 21:04:50 -0700

    Finished main functionality

--- src/App.js
+++ src/App.js
@@ -1,10 +1,73 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
 import { Animated } from "react-animated-css";
+import axios from "axios";
+import InfiniteScroll from "react-infinite-scroll-component";
 
 function App() {
   const [location, setLocation] = useState("");
   const [searched, setSearched] = useState(false);
+  const [temperature, setTemperature] = useState();
+  const [weatherIcon, setWeatherIcon] = useState();
+  const [images, setImages] = useState([]);
+  const [offset, setOffset] = useState(11);
+  const [weatherAfterSearch, setWeatherAfterSearch] = useState("");
+
+  const handleSearch = () => {
+    setSearched(true);
+    axios(
+      `http://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&appid=42ae5bc7019f475d94e548841ac2c7bb`
+    )
+      .then(function (response) {
+        let temp = response.data["main"]["feels_like"];
+        let weather = "";
+        if (temp > 17) {
+          weather = "Summer";
+        } else if (temp < 17 && temp > 12) {
+          weather = "Fall";
+        } else {
+          weather = "Winter";
+        }
+        setWeatherAfterSearch(weather);
+        setTemperature(temp);
+        setWeatherIcon(
+          `http://openweathermap.org/img/wn/${response.data["weather"][0]["icon"]}@2x.png`
+        );
+        axios(
+          `https://www.googleapis.com/customsearch/v1?q=${weather}+outfit+men&num=10&searchType=image&key=AIzaSyCePRC6pC81oWNEa-mqSfDPgYy3x39168E&cx=000700725034361579724:fdsgnzu8lts`
+        )
+          .then(function (response) {
+            let imageResults = response.data["items"];
+            let contextLinks = imageResults.map((currentValue) => {
+              return <img src={currentValue["link"]} alt="clothing" />;
+            });
+            setImages(contextLinks);
+          })
+          .catch(function (error) {
+            alert(error);
+          });
+      })
+      .catch(function (error) {
+        alert(error);
+      });
+  };
+
+  const fetchMoreData = () => {
+    axios(
+      `https://www.googleapis.com/customsearch/v1?q=${weatherAfterSearch}+outfit+men&num=10&searchType=image&start=${offset}&key=AIzaSyCePRC6pC81oWNEa-mqSfDPgYy3x39168E&cx=000700725034361579724:fdsgnzu8lts`
+    )
+      .then(function (response) {
+        let imageResults = response.data["items"];
+        let contextLinks = imageResults.map((currentValue) => {
+          return <img src={currentValue["link"]} alt="clothing" />;
+        });
+        setImages(images.concat(contextLinks));
+        setOffset(offset + 10);
+      })
+      .catch(function (error) {
+        alert(error);
+      });
+  };
 
   useEffect(() => {
     let input = document.getElementById("pac-input");
@@ -18,28 +81,74 @@ function App() {
   });
 
   return (
-    <Animated animationIn="fadeIn" animationOut="fadeOut" isVisible={!searched}>
-      <div id="main-div" className="main-div">
-        <h1>Enter Your Location</h1>
-        <input
-          id="pac-input"
-          className="search-bar"
-          type="text"
-          value={location}
-          onChange={(event) => {
-            setLocation(event.target.value);
-          }}
-        />
-        <input
-          id="submit-btn"
-          className="submit-btn"
-          type="button"
-          onClick={() => {
-            setSearched(true);
-          }}
-        />
-      </div>
-    </Animated>
+    <div className="main-canvas">
+      {!searched && (
+        <Animated
+          animationIn="fadeIn"
+          animationOut="fadeOut"
+          isVisible={!searched}
+        >
+          <div id="main-div" className="main-div">
+            <h1>Enter Your Location</h1>
+            <input
+              id="pac-input"
+              className="search-bar"
+              type="text"
+              value={location}
+              onChange={(event) => {
+                setLocation(event.target.value);
+              }}
+            />
+            <input
+              id="submit-btn"
+              className="submit-btn"
+              type="button"
+              onClick={handleSearch}
+            />
+          </div>
+        </Animated>
+      )}
+      {searched && (
+        <Animated>
+          <div className="parent-main-div-searched">
+            <div className="main-div-searched">
+              <img
+                className="weather-icon"
+                src={weatherIcon}
+                alt="Weather icon"
+              />
+              <h2>{temperature + "°C"}</h2>
+              <input
+                id="pac-input"
+                className="search-bar-searched"
+                type="text"
+                placeholder="Enter Your Location"
+                value={location}
+                onChange={(event) => {
+                  setLocation(event.target.value);
+                }}
+              />
+              <input
+                id="submit-btn"
+                className="submit-btn"
+                type="button"
+                onClick={handleSearch}
+              />
+            </div>
+            <div className="image-gallery">
+              <InfiniteScroll
+                dataLength={images.length}
+                next={fetchMoreData}
+                hasMore={true}
+                loader={<h4>Loading...</h4>}
+              >
+                {images}
+              </InfiniteScroll>
+            </div>
+          </div>
+        </Animated>
+      )}
+    </div>
   );
 }
 
